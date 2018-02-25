fad7eff86de6e43a4caf44eaa6854fb6f62ffd18
codermarcos <codermarcos>
2018-02-24 22:13:30 -0300

    In Progress task #7 -  tryning push via script

--- .travis/push.sh
+++ .travis/push.sh
@@ -3,7 +3,7 @@
 commit() {
   git config user.name "Travis CI"
   git config user.email "$COMMIT_AUTHOR_EMAIL"
-  git checkout -b release origin/release
+  git checkout release
   git add . dist/*
   git commit -m "Release version $(cat package.json | grep version | awk -F\":\" '{print $2}' | sed 's/[\", ]//g')#$TRAVIS_BUILD_NUMBER"
   git push -q https://$access_token@github.com/Developers-SP/my-team-web.git release
